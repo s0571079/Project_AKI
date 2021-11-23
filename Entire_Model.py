14e7d3cea89358947f4f1f728cfb3c146e0fc701
Marius Poppel <Marius.Poppel@Student.htw-berlin.de>
2021-11-23 20:26:20 +0100

    added some question

--- Entire_Model.py
+++ Entire_Model.py
@@ -21,7 +21,7 @@ class Net(nn.Module):
 
         # Network architecture is described here; see network_architecture.png
         # CustomLSTM -> LSTM -> ReLu -> Linear
-        # QUESTION: Why 1 here?; Why same hidden size each here? Hidden size hier 64? Kann ich hidden_size beliebig variieren?
+        # QUESTION: Kann ich hidden_size beliebig variieren?
         self.MI_LSTM_layer = lstm.CustomMultiInputLSTM(5, hidden_size)
         self.StandardLSTM_layer = nn.LSTM(hidden_size, hidden_size)
         self.relu_layer = nn.ReLU()
@@ -31,8 +31,10 @@ class Net(nn.Module):
         # Executed when input is passed into the neural network
         # Put the data through the layers -> CustomLSTM -> LSTM -> ReLu -> Linear
         # Pass sequence as parameter for second layer
-        # output = 1, 64, hidden = 1 22, 64
         output, hidden = self.MI_LSTM_layer(Y, x1, x2, x3, x4, x5, x6, x7, x8)
+        # output = 1, 64 // hidden = 1, 22, 64
+        # QUESTION1: output müsste passen -> da 64 Knoten im Layer; bei hidden passt von shape, wieso 22/64?
+        # QUESTION2: -> Übergabe an nächstes LSTM Output nicht in nächsten LSTM geben?
         output, hidden = self.StandardLSTM_layer(hidden)
         output = self.relu_layer(output)
         output = self.lin_layer(output)
@@ -102,14 +104,15 @@ for epoch in range(10):
     running_loss = 0
 
     # Loop through every record
+    # T = 22
     for Y, y, x1, x2, x3, x4, x5, x6, x7, x8 in loader:
         optimizer.zero_grad()
+        # QUESTION: Input ins Netzwerk: Datenstrukturen
         # Y = 1, 5, 22
-        # x1 = 1, 3 ,22
-        # x2 = gleich
-        # y = 1 Wert (bei batch size 1); wird zu liste der Größe Anzahl batch size
-        print(Y)
-        # Input ins Netzwerk -> (Liste 22x5 (Open,Close,Volume, Min, Max), (Talib-Klasse1), (Talib-Klasse2) ... (Talib-Klasse8)]
+        # x1 = 1, 3 ,22 (bei 3 Kennzahlen pro Klasse)
+        # x2 = 1, 3 ,22
+        # x3 = ....
+        # y = 1 Wert -> Wird eine Liste in der Größe der Anzahl der Batch size?
         outputs = net(Y.float(), x1.float(), x2.float(), x3.float(), x4.float(), x5.float(), x6.float(), x7.float(), x8.float())
         # Vergleich mit labels
         loss = criterion(torch.squeeze(outputs), y.float())
